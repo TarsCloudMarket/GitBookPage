387cc83a87d1f4488988f768106ea76cfe09daad
ruanshudong <ruanshudong@qq.com>
2022-03-01 20:38:33 +0800

    update log

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.proxy = true;
 // error handler
 // onerror(app);
 
-const appInitialize = () => {
+const appInitialize = (conf: any) => {
 
     //安全防护
     app.use(helmet());
@@ -33,22 +33,17 @@ const appInitialize = () => {
     //国际化多语言中间件
     app.use(localeMidware);
 
-    // if (webConf.config.login.enableLogin) {
-    //     app.use(ssoMiddleware(loginConf));
-    // }
-
     app.use(staticRouter(path.join(__dirname, "../client/dist"), { maxage: 7 * 24 * 60 * 60 * 1000 }));
 
-
-    console.log(webConf.config.cn_git);
-    console.log(webConf.config.en_git);
-    app.use(staticRouter(webConf.config.cn_git, { maxage: 7 * 24 * 60 * 60 * 1000 }));
-    app.use(staticRouter(webConf.config.en_git, { maxage: 7 * 24 * 60 * 60 * 1000 }));
+    console.log(conf);
+    // console.log(webConf);
+    app.use(staticRouter(conf.config.cn_git, { maxage: 7 * 24 * 60 * 60 * 1000 }));
+    app.use(staticRouter(conf.config.en_git, { maxage: 7 * 24 * 60 * 60 * 1000 }));
 
     app.use(pageRouter.routes());
     app.use(apiRouter.routes());
 
-    // var _logger = new TarsLogs("TarsRemote");
+    const _logger = new TarsLogs("TarsRemote");
 
     app.use(async (ctx: Koa.Context, next) => {
         await next();
@@ -62,18 +57,12 @@ const appInitialize = () => {
                 maxAge: 86400 * 365 * 1000,
             });
 
-            // _logger.debug(uid + "|" + ctx.request.url);
+            _logger.debug(uid + "|" + ctx.request.url);
 
             const file = path.join(__dirname, 'dist', 'index.html');
             await sendFile(ctx, file);
         }
     });
-    // app.use(ssoRouter.routes());
-
-    // if (webConf.config.login.enableLogin) {
-    //     LoginService.initialize();
-    // }
-
     console.log("initialize markdown");
 
     TreeController.initialize();
@@ -89,16 +78,16 @@ const initialize = async () => {
 
         webConf.config = await tarsConfig.loadConfig(`config.json`, { format: tarsConfig.FORMAT.JSON });
 
-        console.log("initialize, in tars");
+        console.log("initialize, in tars", webConf);
 
     } else {
         webConf.config = JSON.parse(fs.readFileSync(path.join(__dirname, "./config/config.json"), "utf8"));
-        console.log("initialize, not in tars");
+        console.log("initialize, not in tars", webConf);
     }
 
     console.log(webConf);
 
-    appInitialize();
+    appInitialize(webConf);
 }
 
 export { app, initialize };
